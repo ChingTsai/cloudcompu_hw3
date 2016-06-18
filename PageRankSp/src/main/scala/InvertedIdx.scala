7fba00cb7567b98620698335af75f1e045affb1d
MIT Bro <ctsai@netdb.cs.nthu.edu.tw>
2016-06-18 11:32:22 +0800

    New preprocess

--- PageRankSp/src/main/scala/InvertedIdx.scala
+++ PageRankSp/src/main/scala/InvertedIdx.scala
@@ -1,20 +1,13 @@
-import org.apache.spark.SparkConf
-
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.Path
+import org.apache.hadoop.io.IntWritable
+import org.apache.hadoop.io.Text
+import org.apache.hadoop.mapred.TextOutputFormat
 import org.apache.spark.HashPartitioner
-import org.apache.hadoop.io.NullWritable
-import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat
-import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
 
-class RDDMultipleTextOutputFormat extends MultipleTextOutputFormat[Any, Any] {
-  override def generateActualKey(key: Any, value: Any): Any =
-    NullWritable.get()
-
-  override def generateFileNameForKeyValue(key: Any, value: Any, name: String): String =
-    key.asInstanceOf[String]
-}
 
 object InvertedIdx {
   def main(args: Array[String]) {
@@ -30,16 +23,18 @@ object InvertedIdx {
     val hadoopConf = sc.hadoopConfiguration
     var hdfs = FileSystem.get(hadoopConf)
     try { hdfs.delete(new Path(outputPath), true) } catch { case _: Throwable => {} }
-
+    try { hdfs.delete(new Path("Hw3/ids2title"), true) } catch { case _: Throwable => {} }
+    
     val lines = sc.textFile(filePath, sc.defaultParallelism * 3)
 
-   (lines.map(line => {
+   val res =  (lines.map(line => {
       val lineXml = scala.xml.XML.loadString(line.toString())
-      (((lineXml \ "title").text.concat("&gt").concat((lineXml \ "revision" \ "text").text)));
+      ((lineXml \ "title").text,((lineXml \ "revision" \ "text").text));
     }))
     .zipWithUniqueId().map(x => ( x._2.toString(), x._1)).partitionBy(new HashPartitioner(sc.defaultParallelism * 3))
-    .saveAsHadoopFile(outputPath, classOf[String], classOf[String], classOf[RDDMultipleTextOutputFormat])
-
+    res.map(x => (x._1,x._2._2)).saveAsHadoopFile(outputPath,classOf[Text],classOf[Text],classOf[TextOutputFormat[Text,Text]])
+    res.map(x=>(x._1,x._2._1)).saveAsHadoopFile("Hw3/ids2title",classOf[Text],classOf[Text],classOf[TextOutputFormat[Text,Text]])
+    
     sc.stop
   }
 }
