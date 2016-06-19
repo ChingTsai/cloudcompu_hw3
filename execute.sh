ae3797a055e6c884bbe6cc72e93826fc005cec10
MIT Bro <ctsai@netdb.cs.nthu.edu.tw>
2016-06-19 10:53:34 +0800

    Query fix Htable

--- execute.sh
+++ execute.sh
@@ -3,11 +3,4 @@
 #hdfs dfs -rm -r /user/TA/WordCount/Output/
 #hadoop jar WordCount.jar wordcount.WordCount /user/shared/WordCount/Input /user/TA/WordCount/Output
 #hdfs dfs -cat /user/TA/WordCount/Output/part-*
-
-hdfs dfs -rm -r input
-hdfs dfs -put input input
-hdfs dfs -rm -r invidx
-hadoop jar ${PWD##*/}.jar CloudCompu.hw1.InvertedIndex input invidx
-hdfs dfs -cat invidx/part-*
-rm -rf invidx
-hdfs dfs -get invidx .
+hadoop jar Query.jar hw3.query.Query
