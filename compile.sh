f3d725e2fd528019482f1fec82ab72b0823e8a5f
MIT Bro <ctsai@netdb.cs.nthu.edu.tw>
2016-06-19 10:30:26 +0800

    Cleanup and compilre

--- compile.sh
+++ compile.sh
@@ -1,11 +1,11 @@
 # You do not have to modify this file unless you have multiple packages in your MR java code
 printf "Cleaning old src ...\n"
 rm -r code/*
-cp hw1/src/main/java/CloudCompu/hw1/* code 
+cp query/src/main/java/hw3/query/* code 
 printf "Cleaning old class ...\n"
 rm -r class/*
 printf "Java Compile ... \n"
-javac -classpath  $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.7.2.jar:$HADOOP_HOME/share/hadoop/common/hadoop-common-2.7.2.jar -d class code/*
+javac -Xlint -classpath $HADOOP_HOME/share/hadoop/common/hadoop-common-2.7.2.jar:$HBASE_HOME/lib/hbase-common-1.2.1.jar:$HBASE_HOME/lib/hbase-client-1.2.1.jar -d class code/*
 printf "packaging ... \n"
-jar -cvf ${PWD##*/}.jar -C class/ .
+jar -cvf Query.jar -C class/ .
 printf "Done!\n"
