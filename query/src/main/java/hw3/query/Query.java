15c96f37a87e4e24f11f59480f6260736480670e
MIT Bro <ctsai@netdb.cs.nthu.edu.tw>
2016-06-20 13:10:19 +0800

    Test scan

--- query/src/main/java/hw3/query/Query.java
+++ query/src/main/java/hw3/query/Query.java
@@ -165,7 +165,9 @@ public class Query {
 					for (String[] sa : p.offset) {
 						for (String s : sa) {
 							int f = Integer.parseInt(s);
-							if(f!=L.getFirst())
+							if (L.size() == 0)
+								L.add(f);
+							else if (f != L.getFirst())
 								L.add(f);
 							if (L.size() == 3)
 								break;
