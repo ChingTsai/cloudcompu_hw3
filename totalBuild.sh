90f0f591b0d6b9bcb311b14aa82ee387e2d73e6f
MIT Bro <ctsai@netdb.cs.nthu.edu.tw>
2016-06-20 16:33:13 +0800

    Spark to Hbase refactory

--- totalBuild.sh
+++ totalBuild.sh
@@ -7,9 +7,9 @@ time spark-submit --class NewInvIdx --num-executors 30 target/scala-2.10/*.jar H
 time spark-submit --class NewPageRank --num-executors 30 target/scala-2.10/*.jar /shared/HW2/sample-in/input-1G Hw3/PageRank
 cd ..
 cp ./PageRankSp/N.txt .
-#hbase shell ./hbaseShell.txt
+hbase shell ./hbaseShell.txt
 #hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator='^' -Dimporttsv.columns=HBASE_ROW_KEY,text s104062587:preprocess Hw3/preprocess
-#hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator="," -Dimporttsv.columns=HBASE_ROW_KEY,df,info s104062587:invidx Hw3/invidx
+hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator="," -Dimporttsv.columns=HBASE_ROW_KEY,df,info s104062587:invidx Hw3/invidx
 #hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator="|" -Dimporttsv.columns=HBASE_ROW_KEY,pr s104062587:pagerank Hw3/PageRank
 #hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator="|" -Dimporttsv.columns=HBASE_ROW_KEY,title s104062587:ids2title Hw3/ids2title
 #hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator="|" -Dimporttsv.columns=HBASE_ROW_KEY,id s104062587:title2ids Hw3/title2ids
